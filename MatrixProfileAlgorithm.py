c8dbd030e4e71493818684e476e1f37efb3079f3
Szymon Kuc <kucszym@gmail.com>
2026-06-27 13:30:48 +0200

    zmiany

--- MatrixProfileAlgorithm.py
+++ MatrixProfileAlgorithm.py
@@ -1,20 +1,25 @@
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
-def matrix_profile(m,df_clean,str1,str2):
+def matrix_profile(m, df_clean, str1, str2):
     s1 = df_clean[str1].astype(float).values
     s2 = df_clean[str2].astype(float).values
     s1_scaled = (s1 - s1.mean()) / s1.std()
     s2_scaled = (s2 - s2.mean()) / s2.std()
-    n = min(len(s2_scaled),len(s1_scaled))
+    
+    n = min(len(s2_scaled), len(s1_scaled))
+    s1_scaled, s2_scaled = s1_scaled[:n], s2_scaled[:n]
+
+    # tworzy widok wszystkih możliwych okien z szeregu s2 o długości m
+    windows2 = sliding_window_view(s2_scaled, m)
 
     matrix_profile_distances = []
-    for i in range(n - m+1):
-        subseq = s1_scaled[i:i+m] # kawałek od wielkosi m zaczynajacy sie od indeksu i
-        min_dist = float('inf')
-        for j in range(n - m+1):
-            target = s2_scaled[j:j+m]
-            distance = np.sqrt(np.sum((subseq - target)**2))
-            if distance < min_dist:
-                min_dist = distance
-        matrix_profile_distances.append(min_dist)
+    for i in range(n - m + 1):
+        subseq = s1_scaled[i:i+m]
+        # odległość euklidesowa
+        distances = np.linalg.norm(windows2 - subseq, axis=1)
+        # zbyt małe odległości zmieniamy na inf
+        distances[max(0, i - m + 1) : i + m] = np.inf
+        matrix_profile_distances.append(np.min(distances))
+
     return matrix_profile_distances
